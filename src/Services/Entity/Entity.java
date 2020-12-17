54183a6584de65c652a4d2cde6392761cfc0a786
Loïc Roux <47380500+loic-roux-404@users.noreply.github.com>
2020-12-17 14:57:15 +0100

    Update Entity.java

--- src/Services/Entity/Entity.java
+++ src/Services/Entity/Entity.java
@@ -7,6 +7,4 @@ public interface Entity extends Serializable {
      * This method should return ordered comma list separated entity items
      */
     public String toString();
-
-    public Entity factory(String[] dbData);
 }
