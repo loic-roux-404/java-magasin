d76e074acbf66645fa5be71ce6fa4e87adab8685
loic-roux <69loloro10@gmail.com>
2021-01-28 19:39:11 +0100

    prod script

--- .github/production.sh
+++ .github/production.sh
@@ -2,10 +2,10 @@
 
 echo "[ Sync production branch ]"
 git add -A && git stash
-git checkout master
-git pull origin master --rebase
-git push --force-with-lease
-git rebase --onto prod
+git fetch origin master prod
+git checkout origin/prod
+git rebase origin/master
+git push origin prod --force-with-lease
 git checkout -
 git stash pop
 echo "[ Done ]"
